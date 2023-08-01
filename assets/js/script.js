0356becd84a95e45b6cdb2b14208b493ba1f73ed
Chika Dim <chikadim@chikas-mbp.speedport.ip>
2023-08-01 03:50:50 +0200

    Add quiz section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("See");
+const startButton = document.querySelector('.start-button');
+const instruction = document.querySelector('.instruction');
+const quitButton = document.querySelector('.quit-button');
+const mainSection = document.querySelector('.main-section');
+
+startButton.onclick = () => {
+    instruction.classList.add('active');
+    mainSection.classList.add('active');
+}
+
+quitButton.onclick = () => {
+    instruction.classList.remove('active');
+    mainSection.classList.remove('active');
+}
