44d18e2ea35f9a2281d16d02b015378c536bd1ce
Chika Dim <chikadim@chikas-mbp.speedport.ip>
2023-08-01 08:59:03 +0200

    Add quiz box

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const startButton = document.querySelector('.start-button');
 const instruction = document.querySelector('.instruction');
 const quitButton = document.querySelector('.quit-button');
 const mainSection = document.querySelector('.main-section');
+const playNowButton = document.querySelector('.play-now-button');
+const quizSection = document.querySelector('.quiz-section');
 
 startButton.onclick = () => {
     instruction.classList.add('active');
@@ -11,4 +13,11 @@ startButton.onclick = () => {
 quitButton.onclick = () => {
     instruction.classList.remove('active');
     mainSection.classList.remove('active');
+}
+
+playNowButton.onclick = () => {
+    quizSection.classList.add('active');
+    instruction.classList.remove('active');
+    mainSection.classList.remove('active');
+
 }
